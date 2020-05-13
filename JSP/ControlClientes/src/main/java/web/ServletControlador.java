42406951c63c64bb76aba15dea3da31806a48d2e
Jairo Alejandro Castañeda Pedraza <jairo.castaneda@uptc.edu.co>
2020-05-13 13:19:29 -0500

    Totalizando usuarios y saldos

--- JSP/ControlClientes/src/main/java/web/ServletControlador.java
+++ JSP/ControlClientes/src/main/java/web/ServletControlador.java
@@ -28,6 +28,16 @@ public class ServletControlador extends HttpServlet{
         List<Cliente> clientes= new ClienteDaoJDBC().listar();
         System.out.println("clientes= "+clientes);
         request.setAttribute("clientes", clientes);
+        request.setAttribute("totalClientes", clientes.size());
+        request.setAttribute("saldoTotal", caldularSaldoTotal(clientes));
         request.getRequestDispatcher("clientes.jsp").forward(request, response);
+    }
+    private double caldularSaldoTotal(List<Cliente> clientes){
+        double saldoTotal=0;
+        
+        for(Cliente cliente:clientes){
+            saldoTotal+= cliente.getSaldo();
+        }
+        return saldoTotal;
     }
 }
