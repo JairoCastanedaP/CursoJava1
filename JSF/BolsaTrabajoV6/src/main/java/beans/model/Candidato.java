73f36dc8a4da42719ce9de6a38d09c0c550bf90c
Jairo Alejandro Castañeda Pedraza <jairo.castaneda@uptc.edu.co>
2020-05-14 23:34:45 -0500

    ActionListener JSF

---
+++ JSF/BolsaTrabajoV6/src/main/java/beans/model/Candidato.java
@@ -0,0 +1,96 @@
+package beans.model;
+
+import java.util.Date;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Named;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Named
+@RequestScoped
+public class Candidato {
+
+    private String nombre;
+    private String apellido;
+    private int salarioDeseado;
+    private Date fechaNacimiento;
+    private String codigoPostal;
+    private String colonia;
+    private String ciudad;
+    private String comentario;
+
+    Logger log = LogManager.getRootLogger();
+
+    public Candidato() {
+        log.info("Creando objeto Candidato");
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+        log.info("Modificando la propiedad de nombre:" + this.nombre);
+    }
+
+    public String getApellido() {
+        log.info("obteniendo el apellido");
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        log.info("modificando el apellido :" + this.apellido);
+        this.apellido = apellido;
+    }
+
+    public int getSalarioDeseado() {
+        return salarioDeseado;
+    }
+
+    public void setSalarioDeseado(int salarioDeseado) {
+        log.info("Modificando la propiedad salarioDeseado" + this.salarioDeseado);
+        this.salarioDeseado = salarioDeseado;
+    }
+
+    public Date getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(Date fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getCodigoPostal() {
+        return codigoPostal;
+    }
+
+    public void setCodigoPostal(String codigoPostal) {
+        this.codigoPostal = codigoPostal;
+    }
+
+    public String getColonia() {
+        return colonia;
+    }
+
+    public void setColonia(String colonia) {
+        this.colonia = colonia;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    public String getComentario() {
+        return comentario;
+    }
+
+    public void setComentario(String comentario) {
+        this.comentario = comentario;
+    }
+    
+}
