83b9b03f7b592e92b28f198288f66c781144a300
Jairo Alejandro Castañeda Pedraza <jairo.castaneda@uptc.edu.co>
2020-05-19 21:44:28 -0500

    ciclo de vida JPA crud

---
+++ JavaEE/sga_jee_web3/src/main/java/servicio/PersonaServiceImpl.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servicio;
+
+import datos.PersonaDao;
+import domain.Persona;
+import java.util.ArrayList;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+
+/**
+ *
+ * @author ENVYX360
+ */
+@Stateless
+public class PersonaServiceImpl implements PersonaServiceRemote, PersonaService{
+
+    @Inject
+    private PersonaDao personaDao;
+    
+    @Override
+    public List<Persona> listarPersonas() {
+       return personaDao.findAllPersonas();
+    }
+
+    @Override
+    public Persona encontrarPersonaPorId(Persona persona) {
+        return personaDao.findPersonaById(persona);
+    }
+
+    @Override
+    public Persona encontrarPersonaPorEmail(Persona persona) {
+        return personaDao.findPersonaByEmail(persona);
+    }
+
+    @Override
+    public void registrarPersona(Persona persona) {
+        personaDao.insertPersona(persona);
+    }
+
+    @Override
+    public void modificarPersona(Persona persona) {
+        personaDao.updatePersona(persona);
+    }
+
+    @Override
+    public void eliminarPersona(Persona persona) {
+        personaDao.deletePersona(persona);
+    }
+    
+}
